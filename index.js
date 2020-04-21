43655c4ad600cf5fc3f079426c66eb5c529a84d2
harry981 <sharmahs981@gmail.com>
2020-04-21 17:12:32 +0530

    Initialized API

---
+++ index.js
@@ -0,0 +1,13 @@
+var express=require('express')
+
+var app=express();
+
+var PORT=process.env.PORT||3000;
+
+app.get('/',(req,res)=>{
+   res.send("Welcome to my API !!")
+})
+
+app.listen(PORT,()=>{
+    console.log('Running on Port : '+PORT)
+})
